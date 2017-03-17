43a72131f00cb39e0633a09af7c9a727d8f2d817
Jason Watson <jason.jw@live.co.uk>
2017-03-17 01:00:36 +0000

    Create script to calculate gain matching values

---
+++ scripts/calculate_gain_matching.py
@@ -0,0 +1,94 @@
+import iminuit
+
+from targetpipe.io.pixels import Dead
+import numpy as np
+from matplotlib import pyplot as plt
+from traitlets import Dict, List, Unicode
+from ctapipe.core import Tool
+from os.path import exists, join
+from os import makedirs
+import seaborn as sns
+from pandas import DataFrame
+from functools import partial
+
+
+class ADC2PEvsHVPlotter(Tool):
+    name = "ADC2PEvsHVPlotter"
+    description = "For a given hv values, see the conversion from adc " \
+                  "to pe for each pixel."
+
+    gain_matching_path = Unicode('', help='Path to the numpy gain matching '
+                                          'file').tag(config=True)
+    output_dir = Unicode('', help='Directory to store output').tag(config=True)
+
+    aliases = Dict(dict(gm='ADC2PEvsHVPlotter.gain_matching_path',
+                        o='ADC2PEvsHVPlotter.output_dir'
+                        ))
+
+    classes = List([
+                    ])
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.c = None
+        self.alpha = None
+
+    def setup(self):
+        self.log_format = "%(levelname)s: %(message)s [%(name)s.%(funcName)s]"
+        kwargs = dict(config=self.config, tool=self)
+
+        gain_matching = np.load(self.gain_matching_path)
+        self.c = gain_matching['C_tm']
+        self.alpha = gain_matching['alpha_tm']
+
+    def start(self):
+        hv = 800
+        gm_800 = self.gain_match(self.c, self.alpha, hv).astype(np.int)
+        print('[', ', '.join(map(str, gm_800)), ']')
+
+        hv = 900
+        gm_900 = self.gain_match(self.c, self.alpha, hv).astype(np.int)
+        print('[', ', '.join(map(str, gm_900)), ']')
+
+        hv = 1000
+        gm_1000 = self.gain_match(self.c, self.alpha, hv).astype(np.int)
+        print('[', ', '.join(map(str, gm_1000)), ']')
+
+    def finish(self):
+        pass
+
+    @staticmethod
+    def x_function(c, alpha, x):
+        return np.mean(c * np.power(x, alpha))
+
+    @staticmethod
+    def y_function_arr(c, alpha, y):
+        x_val = np.round(np.power(y / c, 1 / alpha))
+        x_val[x_val > 1100] = 1100
+        x_val[x_val < 0] = 0
+        return x_val
+
+    @staticmethod
+    def y_function(c, alpha, y):
+        return np.mean(ADC2PEvsHVPlotter.y_function_arr(c, alpha, y))
+
+    def gain_match(self, c, alpha, goal_x):
+        f_x = partial(self.x_function, c=c, alpha=alpha)
+
+        def m(try_y):
+            return np.abs(self.y_function(c, alpha, try_y) - goal_x)
+
+        p0 = dict(try_y=f_x(x=goal_x))
+        limits = dict(limit_try_y=(f_x(x=0), f_x(x=1500)))
+        m0 = iminuit.Minuit(m, **p0, **limits,
+                            print_level=0, pedantic=False, throw_nan=True)
+        m0.migrad()
+        y = m0.args[0]
+        best_x = self.y_function(c, alpha, y)
+        print("Goal: {}, Best: {}".format(goal_x, best_x))
+        return self.y_function_arr(c, alpha, y)
+
+if __name__ == '__main__':
+    exe = ADC2PEvsHVPlotter()
+    exe.run()
--- scripts/plot_adc2pe_vs_hv.py
+++ scripts/plot_adc2pe_vs_hv.py
@@ -4,8 +4,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from traitlets import Dict, List, Unicode
 from ctapipe.core import Tool
-from os.path import dirname, exists, join
+from os.path import exists, join
 from os import makedirs
+import seaborn as sns
+from pandas import DataFrame
 
 
 class ADC2PEvsHVPlotter(Tool):
@@ -28,27 +30,13 @@ class ADC2PEvsHVPlotter(Tool):
 
         self.a2p = None
         self.dead = None
+
         self.spe = None
+        self.hv = None
+
         self.fig = None
 
-        hv_modules = [[819, 775, 784, 844, 772, 761, 785, 865, 863, 881, 715, 788, 725, 801, 828, 844, 793, 792, 835, 773, 774, 862, 803, 864, 788, 848, 766, 820, 773, 789, 845, 819],
-                      [914, 864, 885, 950, 869, 851, 887, 974, 977, 991, 812, 885, 819, 897, 932, 946, 892, 886, 943, 871, 866, 968, 902, 974, 883, 959, 864, 925, 873, 891, 952, 925],
-                      [1009, 954, 985, 1057, 966, 940, 989, 1084, 1092, 1101, 910, 981, 913, 992, 1035, 1048, 991, 979, 1052, 969, 957, 1075, 1001, 1085, 977, 1071, 963, 1030, 973, 993, 1059, 1032],
-                      [0]*32]
-        shape = (4, 32, 64)
-        self.hv_gm = (np.array(hv_modules)[..., None] * np.ones(shape)).reshape((4, 2048))
-
-        hv_modules = [[800]*2048,
-                      [900]*2048,
-                      [1000]*2048,
-                      [1100]*2048]
-        self.hv_ngm = np.array(hv_modules)
-
-        self.x = [790, 810, 890, 910, 990, 1010, 1090]
-        self.hv = np.empty((self.hv_gm.shape[0] + self.hv_ngm.shape[0], 2048))
-        self.hv[0::2] = self.hv_ngm
-        self.hv[1::2] = self.hv_gm
-        self.hv = self.hv[:-1]
+        sns.set_style("whitegrid")
 
     def setup(self):
         self.log_format = "%(levelname)s: %(message)s [%(name)s.%(funcName)s]"
@@ -57,15 +45,45 @@ class ADC2PEvsHVPlotter(Tool):
         self.a2p = TargetioADC2PECalibrator(**kwargs)
         self.dead = Dead()
 
-    def start(self):
-        self.spe = 1/self.a2p.get_adc2pe_at_hv(self.hv, np.arange(2048)[None, :])
-        self.spe = self.dead.mask2d(self.spe)
+        # hv_modules = [[819, 775, 784, 844, 772, 761, 785, 865, 863, 881, 715, 788, 725, 801, 828, 844, 793, 792, 835, 773, 774, 862, 803, 864, 788, 848, 766, 820, 773, 789, 845, 819],
+        #               [914, 864, 885, 950, 869, 851, 887, 974, 977, 991, 812, 885, 819, 897, 932, 946, 892, 886, 943, 871, 866, 968, 902, 974, 883, 959, 864, 925, 873, 891, 952, 925],
+        #               [1009, 954, 985, 1057, 966, 940, 989, 1084, 1092, 1101, 910, 981, 913, 992, 1035, 1048, 991, 979, 1052, 969, 957, 1075, 1001, 1085, 977, 1071, 963, 1030, 973, 993, 1059, 1032]]
+        hv_modules = [[ 820, 771, 784, 848, 770, 759, 786, 870, 868, 887, 712, 788, 723, 802, 830, 847, 794, 793, 839, 773, 773, 867, 805, 869, 786, 853, 765, 822, 773, 782, 848, 821 ],
+                      [ 916, 860, 885, 954, 866, 848, 887, 980, 982, 997, 809, 885, 816, 898, 934, 949, 893, 886, 947, 871, 864, 973, 904, 979, 882, 964, 864, 928, 873, 883, 955, 928 ],
+                      [ 1011, 949, 986, 1061, 963, 937, 989, 1089, 1098, 1100, 907, 981, 909, 993, 1038, 1052, 992, 979, 1056, 969, 956, 1079, 1003, 1090, 977, 1075, 962, 1033, 973, 985, 1063, 1035 ]]
+        shape = (3, 32, 64)
+        hv_gm = (np.array(hv_modules)[..., None] *
+                 np.ones(shape)).reshape((3, 2048))
+        hv_modules = [[800]*2048,
+                      [900]*2048,
+                      [1000]*2048,
+                      [1100]*2048]
+        hv_ngm = np.array(hv_modules)
+        self.hv = np.vstack((hv_ngm, hv_gm))
 
+    def start(self):
+        adc2pe = self.a2p.get_adc2pe_at_hv(self.hv, np.arange(2048)[None, :])
+        self.spe = self.dead.mask2d(1/adc2pe)
+
+        # Build Dataframe
+        hv_df = np.array([[800]*2048,
+                         [900]*2048,
+                         [1000]*2048,
+                         [1100]*2048,
+                         [800]*2048,
+                         [900]*2048,
+                         [1000]*2048]).flatten()
+        spe_df = self.spe.flatten()
+        gm_df = ['Non-gain-matched']*2048*4 + ['Gain-matched']*2048*3
+        d = dict(hv=hv_df, spe=spe_df, gm=gm_df)
+        df = DataFrame(d)
+
+        # Create Plot
         self.fig = plt.figure(figsize=(13, 6))
         ax = self.fig.add_subplot(1, 1, 1)
-        ax.plot(self.x, self.spe, 'x')
-        ax.set_xlabel('HV (left=Before TM Gain Matching, '
-                      'right=After TM Gain Matching)')
+        sns.violinplot(ax=ax, data=df, x='hv', y='spe', hue='gm',
+                       split=True, scale='count', inner='quartile')
+        ax.set_xlabel('HV')
         ax.set_ylabel('SPE Value (ADC)')
 
     def finish(self):
@@ -78,10 +96,15 @@ class ADC2PEvsHVPlotter(Tool):
         numpy_path = join(output_dir, "spe_vs_hv.npz")
         fig_path = join(output_dir, "spe_vs_hv.pdf")
 
+        x = np.array([790, 890, 990, 1090, 810, 910, 1010])
+        sort = [0, 4, 1, 5, 2, 6, 3]
+        x = x[sort]
+        spe = self.spe[sort]
+
         np.savez(numpy_path,
-                 charge=np.ma.filled(self.spe, 0),
-                 charge_error=np.zeros(self.spe.shape),
-                 rundesc=self.x)
+                 charge=np.ma.filled(spe, 0),
+                 charge_error=np.zeros(spe.shape),
+                 rundesc=x)
         self.log.info("Numpy array saved to: {}".format(numpy_path))
         self.fig.savefig(fig_path)
         self.log.info("Figure saved to: {}".format(fig_path))
