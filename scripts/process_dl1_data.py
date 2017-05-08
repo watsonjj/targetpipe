d15df81f93df30ea12df0b09cf406c21119bdd64
Jason Watson <jason.jw@live.co.uk>
2017-05-08 09:25:38 +0000

    changed process dl1 data script

--- scripts/process_dl1_data.py
+++ scripts/process_dl1_data.py
@@ -1,24 +1,162 @@
 # coding: utf-8
+
 import numpy as np
-a = np.load("/d1/checM/Paris_March2017/Run06992_dl1.npz")
-m = a['baseline_rms_full'].mean(1)
-n = [m[i*10:(i+1)*10].mean() for i in range(int(len(m)/10))]
-plt.plot(n)
 import matplotlib.pyplot as plt
-plt.plot(n)
+
+#laser run
+a = np.load("/d1/checM/Paris_March2017/Run07335_dl1_a.npz")
+
+b = a['baseline_rms_full']
+
+c = np.sqrt((b**2).sum(0))
+
+d = b.mean(0)
+
+e = a['baseline_mean_full']
+f = e.sum(0)
+
+
+
+a1 = np.load("/d1/checM/Paris_March2017/Run07337_dl1_a.npz")
+
+b1 = a1['baseline_rms_full']
+
+c1 = np.sqrt((b1**2).sum(0))
+
+d1 = b1.mean(0)
+
+e1 = a1['baseline_mean_full']
+f1 = e1.sum(0)
+
+
+
+#Initial lid closed, HV on
+a2 = np.load("/d1/checM/Paris_March2017/Run07338_dl1.npz")
+b2 = a2['baseline_rms_full']
+c2 = np.sqrt((b2**2).sum(0))
+
+d2 = b2.sum(0)
+
+e2 = a2['baseline_mean_full']
+f2 = e2.sum(0)
+
+
+fig, axarr = plt.subplots(2)
+
+axarr[0].plot(c-c1,label='uu')
+axarr[1].plot(d-d1)
+
+
+
+#laser run
+a = np.load("/d1/checM/Paris_March2017/Run07340_dl1.npz")
+
+b = a['baseline_rms_full']
+
+c = np.sqrt((b**2).sum(0))
+
+d = b.mean(0)
+
+e = a['baseline_mean_full']
+f = e.sum(0)
+
+
+
+a1 = np.load("/d1/checM/Paris_March2017/Run07341_dl1.npz")
+
+b1 = a1['baseline_rms_full']
+
+c1 = np.sqrt((b1**2).sum(0))
+
+d1 = b1.mean(0)
+
+e1 = a1['baseline_mean_full']
+f1 = e1.sum(0)
+
+axarr[0].plot(c-c1,label='cu')
+axarr[1].plot(d-d1)
+
+
+
+
+
+
+
+
+#laser run
+a = np.load("/d1/checM/Paris_March2017/Run07342_dl1.npz")
+
+b = a['baseline_rms_full']
+
+c = np.sqrt((b**2).sum(0))
+
+d = b.mean(0)
+
+e = a['baseline_mean_full']
+f = e.sum(0)
+
+
+
+a1 = np.load("/d1/checM/Paris_March2017/Run07343_dl1.npz")
+
+b1 = a1['baseline_rms_full']
+
+c1 = np.sqrt((b1**2).sum(0))
+
+d1 = b1.mean(0)
+
+e1 = a1['baseline_mean_full']
+f1 = e1.sum(0)
+
+
+axarr[0].plot(c-c1,label='cc')
+axarr[1].plot(d-d1)
+
+
+
+#laser run
+a = np.load("/d1/checM/Paris_March2017/Run07344_dl1.npz")
+
+b = a['baseline_rms_full']
+
+c = np.sqrt((b**2).sum(0))
+
+d = b.mean(0)
+
+e = a['baseline_mean_full']
+f = e.sum(0)
+
+
+
+a1 = np.load("/d1/checM/Paris_March2017/Run07345_dl1.npz")
+
+b1 = a1['baseline_rms_full']
+
+c1 = np.sqrt((b1**2).sum(0))
+
+d1 = b1.mean(0)
+
+e1 = a1['baseline_mean_full']
+f1 = e1.sum(0)
+
+
+axarr[0].plot(c-c1,label='uc')
+axarr[1].plot(d-d1)
+
+
+
+
+
+axarr[0].set_title('sqrt(sum(RMS_i**2)) subtracted')
+axarr[0].legend()
+axarr[0].set_ylabel('p.e.')
+axarr[1].set_title('MEAN(RMS_i) subtracted')
+axarr[1].set_ylabel('p.e.')
+axarr[1].set_xlabel('Pixel ID')
+#axarr[2].plot(f)
+#axarr[2].set_title('MEAN_i')
+
 plt.show()
-a.keys()
-a['sec']
-len(a['sec'])
-a['ns']
-get_ipython().magic('pinfo np.save')
-get_ipython().magic('pinfo np.save')
-np.save('a.variance',(a['sec'],a['ns'],a['baseline_rms_full'].mean(1)))
-np.savetxt('a.var',(a['sec'],a['ns'],a['baseline_rms_full'].mean(1),a['baseline_rms_full'].std(1)),comments = 'seconds | nanoseconds | baseline RMS mean | baseline RMS mean error')
-np.savetxt('a.var',((a['sec'],a['ns'],a['baseline_rms_full'].mean(1),a['baseline_rms_full'].std(1))),comments = 'seconds | nanoseconds | baseline RMS mean | baseline RMS mean error')
-np.savetxt('a.var',np.transpose([a['sec'],a['ns'],a['baseline_rms_full'].mean(1),a['baseline_rms_full'].std(1)]),comments = 'seconds | nanoseconds | baseline RMS mean | baseline RMS mean error')
-np.savetxt('a.var',np.transpose([a['sec'],a['ns'],a['baseline_rms_full'].mean(1),a['baseline_rms_full'].std(1)]),header = '# seconds | nanoseconds | baseline RMS mean | baseline RMS mean error')
-np.savetxt('a.var',np.transpose([a['sec'],a['ns'],a['baseline_rms_full'].mean(1),a['baseline_rms_full'].std(1)]),header = 'seconds | nanoseconds | baseline RMS mean | baseline RMS mean error')
-import readline
-readline.write_history_file('a')
-get_ipython().magic('save a 1-23')
+
+
+
