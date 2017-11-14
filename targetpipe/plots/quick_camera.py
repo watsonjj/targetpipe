7918760fb8fbf4628f3b949151a1060ab5a37569
Jason Watson <jason.jw@live.co.uk>
2017-11-14 19:27:01 +0000

    Use new TargetCalib Mapping for CHEC-S

--- targetpipe/plots/quick_camera.py
+++ targetpipe/plots/quick_camera.py
@@ -1,6 +1,6 @@
 from ctapipe.instrument import CameraGeometry
 from ctapipe.visualization import CameraDisplay
-from targetpipe.io.pixels import checm_pixel_pos, optical_foclen
+from targetpipe.io.camera import Config
 from astropy import units as u
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,8 +9,9 @@ from matplotlib import pyplot as plt
 def plot_quick_camera(image, ax=None):
     ax = ax if ax is not None else plt.gca()
 
-    pos = checm_pixel_pos * u.m
-    foclen = optical_foclen * u.m
+    cameraconfig = Config()
+    pos = cameraconfig.pixel_pos * u.m
+    foclen = cameraconfig.optical_foclen * u.m
     geom = CameraGeometry.guess(*pos, foclen)
     camera = CameraDisplay(geom, ax=ax, image=image, cmap='viridis')
     return camera
